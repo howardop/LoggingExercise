c9e7d58863b85a25c4aaad78d310d93f055e02dc
howardop <hlo4@cornell.edu>
2021-02-17 19:58:00 -0500

    Added Stream calls to __init__.py

--- FlaskExercise/__init__.py
+++ FlaskExercise/__init__.py
@@ -6,5 +6,8 @@ wsgi_app = app.wsgi_app
 # TODO: Set the app's logger level to "warning"
 #   and any other necessary changes
 app.logger.setLevel(logging.WARNING)
+streamHandler = logging.StreamHandler()
+streamHandler.setLevel(logging.WARNING)
+app.logger.addHandler(streamHandler)
 
 import FlaskExercise.views
